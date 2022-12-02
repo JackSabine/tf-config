5b041243de35773a7c532c7e161ee9398a02f5b2
Jack Sabine <jack@sabine.ws>
2022-12-02 11:11:18 -0500

    Add HUD and support for stow when installing

--- mksymlinks.sh
+++ mksymlinks.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-TF2CFG_DIR="/home/${USER}/.steam/debian-installation/steamapps/common/Team Fortress 2/tf/cfg"
+TF_DIR="/home/${USER}/.steam/debian-installation/steamapps/common/Team Fortress 2/tf/"
 
-stow -t "${TF2CFG_DIR}" -d "${SCRIPT_DIR}" .
+stow -t "${TF_DIR}" -d "${SCRIPT_DIR}" .
